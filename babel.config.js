1f4394f76d2c437878d65fb4cbc40e169a81193a
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-06-01 18:48:29 -0300

    chore: add module-alias

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,19 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+		plugins: [
+      [require.resolve("babel-plugin-module-resolver"), {
+        root: ["."],
+        extensions: [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+        ],
+        alias: {
+          "@": "./src",
+        }
+      }],
+    ],
   };
 };
