29924f40921fca1282184b8622a84f65b37480f8
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-06-01 18:59:48 -0300

    chore: add styled-components

--- App.tsx
+++ App.tsx
@@ -2,15 +2,20 @@ import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { ThemeProvider } from "styled-components/native";
+
+import { theme } from "@/styles/theme";
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			<View style={styles.container}>
-				<Text>Open up App.tsx to start working on your app!</Text>
-				<StatusBar style="auto" />
-			</View>
-		</NavigationContainer>
+		<ThemeProvider theme={theme}>
+			<NavigationContainer>
+				<View style={styles.container}>
+					<Text>Open up App.tsx to start working on your app!</Text>
+					<StatusBar style="light" />
+				</View>
+			</NavigationContainer>
+		</ThemeProvider>
 	);
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,7 @@
+export type Theme = typeof theme;
+
+export const theme = {
+	colors: {
+		font: "#f2f2f2",
+	},
+};
