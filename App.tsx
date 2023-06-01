dce8432fdb9a07179aa0871fb1f25aa5d5a492e3
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-06-01 18:02:50 -0300

    chore: add navigation deps

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<NavigationContainer>
+			<View style={styles.container}>
+				<Text>Open up App.tsx to start working on your app!</Text>
+				<StatusBar style="auto" />
+			</View>
+		</NavigationContainer>
 	);
 }
 
