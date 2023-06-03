e4018656051784d4fd55a875ac03cd4c58ce7cb5
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-06-03 19:26:30 -0300

    chore: add theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,46 @@ export type Theme = typeof theme;
 
 export const theme = {
 	colors: {
-		font: "#f2f2f2",
+		main: "#111212",
+		brand: "#DD9EF3",
+		overlay: "#333333",
+		font: {
+			primary: "#F6F7F7",
+			secondary: "#A2A6A6",
+		},
+		utils: {
+			blue: "#93C4FF",
+			purple: "#C64BFF",
+		},
+		feedbacks: {
+			alert: "#E16161",
+		},
+	},
+	fontsFamily: {
+		main: {
+			semibold: "Roboto_600SemiBold",
+			medium: "Roboto_500Medium",
+			regular: "Roboto_400Regular",
+		},
+	},
+	fontSizes: {
+		display: "40px",
+		lg: "24px",
+		md: "20px",
+		regular: "16px",
+		sm: "14px",
+		xs: "12px",
+	},
+	spaces: {
+		1: "8px",
+		2: "12px",
+		3: "16px",
+		4: "32px",
+		5: "64px",
+	},
+	rounded: {
+		sm: "2px",
+		regular: "16px",
+		md: "32px",
 	},
 };
