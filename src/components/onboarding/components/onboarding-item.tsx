f52c9eb007c29b1640e9b8809ab758a427a4cc80
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-06-16 19:02:01 -0300

    chore: add styles to onboarding-item component

--- src/components/onboarding/components/onboarding-item.tsx
+++ src/components/onboarding/components/onboarding-item.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { Typography } from "../../commons";
 
 export type OnboardingItemProps = {
@@ -15,20 +15,46 @@ export const OnboardingItem = (props: OnboardingItemProps) => {
 	return (
 		<Container>
 			<Image
+				resizeMode="contain"
 				source={typeof img.src === "string" ? { uri: img.src } : img.src}
 				alt={img.alt}
 				accessibilityLabel={img.alt}
 			/>
 			<TextContainer>
-				<Typography.Title>{title}</Typography.Title>
-				<Typography.Paragraph>{description}</Typography.Paragraph>
+				<Title>{title}</Title>
+				<Description>{description}</Description>
 			</TextContainer>
 		</Container>
 	);
 };
 
-const Container = styled.View``;
+const Container = styled.View`
+	${({ theme }) => css`
+		align-items: center;
+		gap: ${theme.spaces[4]};
+	`}
+`;
 
-const Image = styled.Image``;
+const Image = styled.Image`
+	width: 100%;
+	max-width: 320px;
+	max-height: 250px;
+	height: 50%;
+`;
 
-const TextContainer = styled.View``;
+const TextContainer = styled.View`
+	${({ theme }) => css`
+		align-items: center;
+		gap: ${theme.spaces[3]};
+	`}
+`;
+
+const Title = styled(Typography.Title)`
+	max-width: 60%;
+	text-align: center;
+`;
+
+const Description = styled(Typography.Paragraph)`
+	text-align: center;
+	max-width: 350px;
+`;
