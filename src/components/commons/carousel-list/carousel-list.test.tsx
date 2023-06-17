03b1421946caa853ef5a53f888011f34342e0ef0
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-06-17 18:04:58 -0300

    feat: add carousel-list component

---
+++ src/components/commons/carousel-list/carousel-list.test.tsx
@@ -0,0 +1,36 @@
+import { screen } from "@testing-library/react-native";
+import { Text } from "react-native";
+
+import { CarouselList, type CarouselListProps } from "./carousel-list";
+import { renderWithThemeProvider } from "@/__mocks__/render-with-theme-provider";
+
+type Data = { text: string };
+type Props = Omit<CarouselListProps<Data>, "Item">;
+
+const renderComponent = (props: Props) =>
+	renderWithThemeProvider(
+		<CarouselList<Data>
+			{...props}
+			Item={(props) => <Text>{props.text}</Text>}
+		/>
+	);
+
+describe("<CarouselList />", () => {
+	describe("Render", () => {
+		it("should render correctly", () => {
+			const data: Data[] = [{ text: "any_item_0" }, { text: "any_item_1" }];
+			const dataAmount = data.length;
+			const ACTIVE_ITEM = 1;
+			renderComponent({ data, activeItem: ACTIVE_ITEM });
+
+			const indicators = screen.getAllByRole("button");
+			const carouselProps = screen.getByTestId("carousel").props;
+
+			expect(carouselProps.accessibilityValue.max).toBe(data.length);
+			expect(carouselProps.accessibilityValue.min).toBe(1);
+			expect(carouselProps.accessibilityValue.now).toBe(ACTIVE_ITEM);
+			expect(indicators).toHaveLength(dataAmount);
+			expect(screen.getByText(data[0].text)).toBeTruthy();
+		});
+	});
+});
---
+++ src/components/commons/carousel-list/carousel-list.tsx
@@ -0,0 +1,36 @@
+import { FlatList } from "react-native";
+import { Indicator } from "./components/indicator";
+
+export type CarouselListProps<TData extends {}> = {
+	data: TData[];
+	activeItem: number;
+	Item: (props: TData) => JSX.Element;
+};
+
+export const CarouselList = <TData extends object>(
+	props: CarouselListProps<TData>
+) => {
+	const { activeItem, data, Item } = props;
+	const dataAmount = data.length;
+	return (
+		<FlatList<TData>
+			data={data}
+			testID="carousel"
+			accessibilityValue={{ max: dataAmount, min: 1, now: activeItem }}
+			renderItem={({ item }) => <Item {...item} />}
+			ListFooterComponent={() => (
+				<>
+					{[...new Array(dataAmount)].map((_, index) => (
+						<Indicator
+							key={index}
+							currentPosition={index + 1}
+							dataAmount={dataAmount}
+							isActive={index + 1 === activeItem}
+							handlePress={(item) => console.log(item)}
+						/>
+					))}
+				</>
+			)}
+		/>
+	);
+};
