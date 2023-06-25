fe56585618dfa8c80715bfb44f1d2a713b4482b9
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-06-25 17:54:23 -0300

    refactor: increases reusability of component container-with-default-spaces

--- src/components/commons/ContainerWithHorizontalSpacing.ts
+++
@@ -1,8 +0,0 @@
-import styled, { css } from "styled-components/native";
-
-export const ContainerWithHorizontalSpacing = styled.View`
-	${({ theme }) => css`
-		padding-left: ${theme.spaces[3]};
-		padding-right: ${theme.spaces[3]};
-	`}
-`;
