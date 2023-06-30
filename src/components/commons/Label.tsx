a1212ab730134aa30e86440db39913f655088728
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-06-30 18:32:36 -0300

    feat: add label component

---
+++ src/components/commons/Label.tsx
@@ -0,0 +1,18 @@
+import styled, { css } from "styled-components/native";
+import type { ReactNode } from "react";
+
+import { Typography } from "./Typography";
+
+type LabelProps = { children: ReactNode; id: string };
+
+export const Label = (props: LabelProps) => {
+	const { id, children } = props;
+	return <Container nativeID={id}>{children}</Container>;
+};
+
+const Container = styled(Typography.Paragraph)`
+	${({ theme }) => css`
+		color: ${theme.colors.font.primary};
+		font-family: ${theme.fontFamily.main.medium};
+	`}
+`;
