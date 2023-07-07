89ec8c532dade3cf5415a108155e0ebfa91384b7
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-07-07 16:53:36 -0300

    feat: add small-button component

---
+++ src/components/commons/buttons/SmallButton.ts
@@ -0,0 +1,18 @@
+import styled, { css } from "styled-components/native";
+import { BaseButton } from "./BaseButton";
+
+const DEFAULT_SIZE = "44px";
+
+export const SmallButton = styled(BaseButton)`
+	${({ theme }) => css`
+		width: ${DEFAULT_SIZE};
+		max-width: ${DEFAULT_SIZE};
+		height: ${DEFAULT_SIZE};
+		max-height: auto;
+		min-height: auto;
+		align-items: center;
+		justify-content: center;
+		padding: 0px;
+		border-radius: ${theme.rounded.regular};
+	`}
+`;
--- src/components/commons/buttons/index.ts
+++ src/components/commons/buttons/index.ts
@@ -1,2 +1,3 @@
 export * from "./BaseButton";
 export * from "./BackButton";
+export * from "./SmallButton";
