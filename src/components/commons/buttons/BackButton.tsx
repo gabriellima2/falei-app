4537ae90caca6b9a196da2686d444c3720d035f5
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-07-07 16:54:04 -0300

    chore: update styles of the back-button component

--- src/components/commons/buttons/BackButton.tsx
+++ src/components/commons/buttons/BackButton.tsx
@@ -1,24 +1,15 @@
-import styled, { css, useTheme } from "styled-components/native";
+import { useTheme } from "styled-components/native";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { useNavigation } from "expo-router";
+
+import { SmallButton } from "./SmallButton";
 
 export const BackButton = () => {
 	const { colors } = useTheme();
 	const { goBack } = useNavigation();
 	return (
-		<Button onPress={goBack} activeOpacity={0.7}>
+		<SmallButton secondary onPress={goBack} activeOpacity={0.7}>
 			<AntDesign name="arrowleft" color={colors.font.primary} size={20} />
-		</Button>
+		</SmallButton>
 	);
 };
-
-const Button = styled.TouchableOpacity`
-	${({ theme }) => css`
-		width: 40px;
-		height: 40px;
-		align-items: center;
-		justify-content: center;
-		border: 1px solid ${theme.colors.overlay};
-		border-radius: 1000px;
-	`}
-`;
