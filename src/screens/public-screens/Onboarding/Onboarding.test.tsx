46de19c104f23d5dc4c7f098c138c1ce4b405238
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-07-23 18:27:41 -0300

    test: improves test organization of onboarding screen

--- src/screens/public-screens/Onboarding/Onboarding.test.tsx
+++ src/screens/public-screens/Onboarding/Onboarding.test.tsx
@@ -5,9 +5,7 @@ import { Onboarding } from "./Onboarding";
 import { renderWithThemeProvider } from "@/__mocks__/render-with-theme-provider";
 import { onboardingItems } from "./assets";
 
-const CAROUSEL_TESTID = "carousel";
 const NEXT_TEXT = "Próximo";
-const CONTINUE_TEXT = "Continuar";
 const NEXT_BUTTON_HINT = "Move para o próximo item";
 const CONTINUE_BUTTON_HINT = "Redireciona para a tela de autenticação";
 const BACK_BUTTON_HINT = "Move para o item anterior";
@@ -15,19 +13,18 @@ const BACK_BUTTON_HINT = "Move para o item anterior";
 const itemsAmount = onboardingItems.length - 1;
 
 const renderComponent = () => renderWithThemeProvider(<Onboarding />);
-const press = (element: JSX.Element) => fireEvent.press(element);
+
+const getContinueButtonEl = () => screen.getByHintText(CONTINUE_BUTTON_HINT);
+const getBackButtonEl = () => screen.getByHintText(BACK_BUTTON_HINT);
+const getNextButtonEl = () => screen.getByHintText(NEXT_BUTTON_HINT);
+const getCarousel = () => screen.getByTestId("carousel");
 
 describe("<Onboarding", () => {
-	const getBackButtonEl = () => screen.getByHintText(BACK_BUTTON_HINT);
-	const getNextButtonEl = () => screen.getByHintText(NEXT_BUTTON_HINT);
-	const getContinueButtonEl = () => screen.getByHintText(CONTINUE_BUTTON_HINT);
-	const getCarouselProps = () => screen.getByTestId(CAROUSEL_TESTID).props;
-
 	describe("Render", () => {
 		it("should render correctly", () => {
 			renderComponent();
 
-			expect(screen.getByTestId(CAROUSEL_TESTID)).toBeTruthy();
+			expect(getCarousel()).toBeTruthy();
 			expect(screen.getAllByRole("button")[itemsAmount]).toBeTruthy();
 			expect(screen.getByText(NEXT_TEXT)).toBeTruthy();
 			expect(getBackButtonEl()).toBeTruthy();
@@ -36,22 +33,24 @@ describe("<Onboarding", () => {
 	});
 	describe("Interactions", () => {
 		describe("Press", () => {
+			const CONTINUE_TEXT = "Continuar";
+
 			describe("Next", () => {
 				it("should move to next item when next-button is clicked", () => {
 					renderComponent();
 
 					const button = getNextButtonEl();
-					press(button);
+					fireEvent.press(button);
 
-					expect(getCarouselProps().accessibilityValue.now).toBe(1);
+					expect(getCarousel().props.accessibilityValue.now).toBe(1);
 				});
 				it("should move to last item when next-button is clicked repeatedly", () => {
 					renderComponent();
 
 					const button = getNextButtonEl();
-					[...new Array(itemsAmount)].forEach(() => press(button));
+					[...new Array(itemsAmount)].forEach(() => fireEvent.press(button));
 
-					expect(getCarouselProps().accessibilityValue.now).toBe(itemsAmount);
+					expect(getCarousel().props.accessibilityValue.now).toBe(itemsAmount);
 					expect(screen.getByText(CONTINUE_TEXT)).toBeTruthy();
 					expect(getContinueButtonEl()).toBeTruthy();
 				});
@@ -62,7 +61,7 @@ describe("<Onboarding", () => {
 
 					const button = getBackButtonEl();
 					expect(button.props.accessibilityState.disabled).toBeTruthy();
-					press(button);
+					fireEvent.press(button);
 
 					expect(
 						getBackButtonEl().props.accessibilityState.disabled
@@ -71,10 +70,10 @@ describe("<Onboarding", () => {
 				it("should move to previous item when back-button is clicked", () => {
 					renderComponent();
 
-					press(getNextButtonEl());
+					fireEvent.press(getNextButtonEl());
 					const button = getBackButtonEl();
 					expect(button.props.accessibilityState.disabled).toBeFalsy();
-					press(button);
+					fireEvent.press(button);
 
 					expect(
 						getBackButtonEl().props.accessibilityState.disabled
