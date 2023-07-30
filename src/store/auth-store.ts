6f1a7a452007dcbd4b0bd474eaa7c4276e3e0137
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-07-30 12:57:23 -0300

    feat: add sign-out method to auth-store

--- src/store/auth-store.ts
+++ src/store/auth-store.ts
@@ -1,16 +1,22 @@
-import { firebaseAuth } from "@/helpers/firebase-auth";
-import { Unsubscribe, User, onAuthStateChanged } from "firebase/auth";
+import { Unsubscribe, User, onAuthStateChanged, signOut } from "firebase/auth";
 import { create } from "zustand";
+
+import { firebaseAuth } from "@/helpers/firebase-auth";
 
 type UseAuthStore = {
 	user: User | null;
 	authHasBeenChecked: boolean;
+	signOut: () => Promise<void>;
 	checkAuthState: () => Unsubscribe;
 };
 
 export const useAuthStore = create<UseAuthStore>((set) => ({
 	user: null,
 	authHasBeenChecked: false,
+	signOut: async () => {
+		await signOut(firebaseAuth);
+		set((state) => ({ ...state, user: null, authHasBeenChecked: false }));
+	},
 	checkAuthState: () =>
 		onAuthStateChanged(firebaseAuth, (user) => {
 			set((state) => ({ ...state, user, authHasBeenChecked: true }));
