6be52f769309ab30eced4d31158eefc38c96112f
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-08-26 19:27:10 -0300

    chore: add styles to bottom-sheet component

--- src/components/commons/BottomSheet.tsx
+++ src/components/commons/BottomSheet.tsx
@@ -3,6 +3,7 @@ import { type SharedValue } from "react-native-reanimated";
 import BottomSheetLib, {
 	type BottomSheetProps as BottomSheetLibProps,
 } from "@gorhom/bottom-sheet";
+import { useTheme } from "styled-components/native";
 
 type BottomSheetProps = Omit<BottomSheetLibProps, "snapPoints"> & {
 	snapPoints?: (string | number)[] | SharedValue<(string | number)[]>;
@@ -11,6 +12,7 @@ type BottomSheetProps = Omit<BottomSheetLibProps, "snapPoints"> & {
 export const BottomSheet = forwardRef<BottomSheetLib, BottomSheetProps>(
 	(props, ref) => {
 		const { snapPoints } = props;
+		const { colors } = useTheme();
 		const defaultSnapPoints = useMemo(() => [1, "60%"], []);
 
 		return (
@@ -19,6 +21,13 @@ export const BottomSheet = forwardRef<BottomSheetLib, BottomSheetProps>(
 				ref={ref}
 				index={0}
 				enablePanDownToClose
+				backgroundStyle={{ backgroundColor: `${colors.utils.darkGray}` }}
+				style={{ paddingVertical: 8, paddingHorizontal: 16 }}
+				handleIndicatorStyle={{
+					backgroundColor: colors.overlay,
+					width: "40%",
+					height: 8,
+				}}
 				snapPoints={snapPoints ? snapPoints : defaultSnapPoints}
 			/>
 		);
