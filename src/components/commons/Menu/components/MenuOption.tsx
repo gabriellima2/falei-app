b2f16b7508c4b83d9baa52188542062e8a586189
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-08-27 19:11:55 -0300

    chore: add styles to menu-option component

--- src/components/commons/Menu/components/MenuOption.tsx
+++ src/components/commons/Menu/components/MenuOption.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 import { Typography } from "../../Typography";
 import type { MenuOption as TMenuOption } from "@/contexts/MenuContext/@types/menu-option";
@@ -8,15 +8,36 @@ export type MenuOptionProps = TMenuOption;
 export const MenuOption = (props: MenuOptionProps) => {
 	const { text, onPress, icon } = props;
 	return (
-		<Container onPress={onPress}>
-			{icon && <Icon>{icon({ color: "#fff", size: 24 })}</Icon>}
+		<Container onPress={onPress} activeOpacity={0.8}>
+			{icon && <Icon>{icon({ color: "#fff", size: 20 })}</Icon>}
 			<Text>{text}</Text>
 		</Container>
 	);
 };
 
-const Container = styled.TouchableOpacity``;
+const Container = styled.TouchableOpacity`
+	${({ theme }) => css`
+		flex-direction: row;
+		align-items: center;
+		gap: ${theme.spaces[3]};
+		margin-bottom: ${theme.spaces[2]};
+	`}
+`;
 
-const Icon = styled.View``;
+const Icon = styled.View`
+	${({ theme }) => css`
+		width: 44px;
+		height: 44px;
+		align-items: center;
+		justify-content: center;
+		border-radius: ${theme.rounded.regular};
+		border: 1.5px solid ${theme.colors.overlay};
+		background-color: ${theme.colors.utils.darkGray};
+	`}
+`;
 
-const Text = styled(Typography.Title)``;
+const Text = styled(Typography.Title)`
+	${({ theme }) => css`
+		font-size: ${theme.fontSizes.regular};
+	`}
+`;
