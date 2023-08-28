3bb9c9bbe0a0709b7b95cd62f8c5d1f7025b4b2f
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-08-28 19:14:29 -0300

    refactor: add icons and new texts to menu-options

--- src/components/Exercises/ExerciseReminder/components/ExerciseReminderMenu.tsx
+++ src/components/Exercises/ExerciseReminder/components/ExerciseReminderMenu.tsx
@@ -1,13 +1,32 @@
 import styled from "styled-components/native";
-import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 import { useMenuContext } from "@/contexts/MenuContext";
+import { MenuOption } from "@/contexts/MenuContext/@types/menu-option";
+
+const menuOptions: MenuOption[] = [
+	{
+		text: "Editar agendamento",
+		onPress: () => console.log("Editando..."),
+		icon: (props) => <MaterialIcons name="schedule" {...props} />,
+	},
+	{
+		text: "Desativar notificações",
+		onPress: () => console.log("Silenciando..."),
+		icon: (props) => <MaterialIcons name="notifications-off" {...props} />,
+	},
+	{
+		text: "Excluir do agendamento",
+		onPress: () => console.log("Removendo..."),
+		icon: (props) => <MaterialIcons name="highlight-off" {...props} />,
+	},
+];
 
 export const ExerciseReminderMenu = () => {
 	const { handleToggle } = useMenuContext();
 	return (
-		<Button onPress={() => handleToggle([{ name: "any" }])}>
-			<Ionicons name="ellipsis-vertical" size={24} color="#000" />
+		<Button onPress={() => handleToggle(menuOptions)}>
+			<MaterialIcons name="more-vert" size={24} color="#000" />
 		</Button>
 	);
 };
