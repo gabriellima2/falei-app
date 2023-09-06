8a445f5aa6c1ff7e807f47e912e762bfce211361
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-09-05 21:46:32 -0300

    feat: add styles to base-exercise component

--- src/components/Exercises/BaseExercise/BaseExercise.tsx
+++ src/components/Exercises/BaseExercise/BaseExercise.tsx
@@ -1,5 +1,5 @@
 import { type ReactNode } from "react";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 import { BaseLink, BaseLinkProps, Typography } from "@/components/commons";
 
@@ -23,17 +23,42 @@ export const BaseExercise = <TParams extends object>(
 		<Container href={href}>
 			<Icon>{icon({ color: theme.colors.utils.white, size: 24 })}</Icon>
 			<Content>
-				<Title>{title}</Title>
+				<Title subtitle numberOfLines={5}>
+					{title}
+				</Title>
 				{children}
 			</Content>
 		</Container>
 	);
 };
 
-const Container = styled(BaseLink)``;
+const Container = styled(BaseLink)`
+	${({ theme }) => css`
+		width: 50%;
+		max-width: 190px;
+		max-height: 266px;
+		gap: ${theme.spaces[4]};
+		padding: ${theme.spaces[4]} ${theme.spaces[3]};
+		border: 1px solid ${theme.colors.overlay};
+		border-radius: ${theme.rounded.md};
+		background-color: ${theme.colors.utils.darkGray};
+	`}
+`;
 
-const Icon = styled.View``;
+const Icon = styled.View`
+	${({ theme }) => css`
+		width: 44px;
+		height: 44px;
+		align-items: center;
+		justify-content: center;
+		border: 1px solid ${theme.colors.overlay};
+		border-radius: ${theme.rounded.regular};
+		background-color: ${theme.colors.utils.white}0a;
+	`}
+`;
 
-const Content = styled.View``;
+const Content = styled.View`
+	gap: 16px;
+`;
 
 const Title = styled(Typography.Title)``;
