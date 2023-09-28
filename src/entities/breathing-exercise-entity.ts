62dc9d8d8ad88ad4a768572173cbaced2eaab7c2
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-09-28 18:47:45 -0300

    refactor: updates breathing-exercise-entity attrs

--- src/entities/breathing-exercise-entity.ts
+++ src/entities/breathing-exercise-entity.ts
@@ -1,6 +1,19 @@
-export interface BreathingExerciseEntity {
-	id: string;
-	title: string;
-	repetitions: number;
-	duration_in_minutes: number;
+import { GenericExerciseEntity } from "./generic-exercise.entity";
+import { UserEntity } from "./user-entity";
+
+interface Rounds {
+	rounds_total: number;
+	rounds_completed: number;
+	duration_per_round_in_min: number;
+}
+
+export class BreathingExerciseEntity extends GenericExerciseEntity {
+	constructor(
+		id: string,
+		user_id: Pick<UserEntity, "id">,
+		public readonly title: string,
+		public readonly rounds: Rounds
+	) {
+		super(id, user_id);
+	}
 }
---
+++ src/entities/generic-exercise.entity.ts
@@ -0,0 +1,8 @@
+import { UserEntity } from "./user-entity";
+
+export class GenericExerciseEntity {
+	constructor(
+		public readonly id: string,
+		public readonly user_id: Pick<UserEntity, "id">
+	) {}
+}
