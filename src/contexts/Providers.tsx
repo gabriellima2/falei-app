8d49760e91c7b3c5cd51ad5c8d63481af498e3d6
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-09-29 16:41:52 -0300

    build: adds react-query

--- src/contexts/Providers.tsx
+++ src/contexts/Providers.tsx
@@ -1,7 +1,10 @@
 import type { ReactNode } from "react";
+import { QueryClientProvider } from "react-query";
 
 import { ToastProvider } from "./ToastContext";
 import { MenuProvider } from "./MenuContext";
+
+import { queryClient } from "@/services/query-client";
 
 type ProvidersProps = { children: ReactNode };
 
@@ -9,7 +12,11 @@ export const Providers = (props: ProvidersProps) => {
 	const { children } = props;
 	return (
 		<ToastProvider>
-			<MenuProvider>{children}</MenuProvider>
+			<MenuProvider>
+				<QueryClientProvider client={queryClient}>
+					{children}
+				</QueryClientProvider>
+			</MenuProvider>
 		</ToastProvider>
 	);
 };
