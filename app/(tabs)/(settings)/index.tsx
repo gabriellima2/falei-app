35ed80c1a5013dcbec0c998a4b4fe106d8cdadfe
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-10-27 18:45:14 -0300

    refactor: separates screen from the page, creating settings screen

--- app/(tabs)/(settings)/index.tsx
+++ app/(tabs)/(settings)/index.tsx
@@ -1,57 +1,11 @@
-import { Bell, HelpCircle, User2 } from "lucide-react-native";
-
-import {
-	Header,
-	LogoutButton,
-	ScrollContainer,
-	SettingOption,
-	Typography,
-} from "@/components";
-import styled, { css } from "styled-components/native";
+import { Header } from "@/components";
+import { Settings } from "@/screens/private-screens";
 
 export default function Page() {
 	return (
 		<>
 			<Header title="Configurações" />
-			<ScrollContainer>
-				<Content>
-					<Options>
-						<SettingOption
-							text="Minha Conta"
-							href={{ pathname: "/account" }}
-							icon={(props) => <User2 {...props} />}
-						/>
-						<SettingOption
-							text="Notificações"
-							href={{ pathname: "/notifications" }}
-							icon={(props) => <Bell {...props} />}
-						/>
-						<SettingOption
-							text="Sobre"
-							href={{ pathname: "/about" }}
-							icon={(props) => <HelpCircle {...props} />}
-						/>
-						<LogoutButton onLogout={() => console.log("Saindo...")} />
-					</Options>
-					<CreatedBy>© Gabriel Lima</CreatedBy>
-				</Content>
-			</ScrollContainer>
+			<Settings />
 		</>
 	);
 }
-
-const Content = styled.View`
-	${({ theme }) => css`
-		gap: ${theme.spaces[5]};
-	`}
-`;
-
-const Options = styled.View`
-	${({ theme }) => css`
-		gap: ${theme.spaces[3]};
-	`}
-`;
-
-const CreatedBy = styled(Typography.Small)`
-	align-self: center;
-`;
---
+++ src/screens/private-screens/Settings/Settings.tsx
@@ -0,0 +1,55 @@
+import styled, { css } from "styled-components/native";
+import { Bell, HelpCircle, User2 } from "lucide-react-native";
+
+import {
+	LogoutButton,
+	ScrollContainer,
+	SettingOption,
+	Typography,
+} from "@/components";
+import { useAuthStore } from "@/store/auth-store";
+
+export const Settings = () => {
+	const { signOut } = useAuthStore((state) => state);
+	return (
+		<ScrollContainer>
+			<Content>
+				<Options>
+					<SettingOption
+						text="Minha Conta"
+						href={{ pathname: "/account" }}
+						icon={(props) => <User2 {...props} />}
+					/>
+					<SettingOption
+						text="Notificações"
+						href={{ pathname: "/notifications" }}
+						icon={(props) => <Bell {...props} />}
+					/>
+					<SettingOption
+						text="Sobre"
+						href={{ pathname: "/about" }}
+						icon={(props) => <HelpCircle {...props} />}
+					/>
+					<LogoutButton onLogout={signOut} />
+				</Options>
+				<CreatedBy>© Gabriel Lima</CreatedBy>
+			</Content>
+		</ScrollContainer>
+	);
+};
+
+const Content = styled.View`
+	${({ theme }) => css`
+		gap: ${theme.spaces[5]};
+	`}
+`;
+
+const Options = styled.View`
+	${({ theme }) => css`
+		gap: ${theme.spaces[3]};
+	`}
+`;
+
+const CreatedBy = styled(Typography.Small)`
+	align-self: center;
+`;
