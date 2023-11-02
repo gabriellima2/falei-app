073339fae0ada541a6a099f250f31159b4de4329
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-11-02 18:55:14 -0300

    chore: adds styles to check components

--- src/components/commons/Check/Check.tsx
+++ src/components/commons/Check/Check.tsx
@@ -1,31 +1,29 @@
-import styled from "styled-components/native";
-
 import { Option, type OptionProps } from "./components";
 import { useCheck, type UseCheckParams } from "./hooks/use-check";
 
-export type CheckProps = UseCheckParams & {
-	items: Pick<OptionProps, "name" | "value">[];
-};
+export type CheckProps = UseCheckParams &
+	Pick<OptionProps, "style"> & {
+		items: Pick<OptionProps, "name" | "value">[];
+	};
 
 export const Check = (props: CheckProps) => {
-	const { items, initialValue, multipleValues, onChange } = props;
+	const { items, initialValue, multipleValues, onChange, ...rest } = props;
 	const { handlePress, isChecked } = useCheck({
 		initialValue,
 		multipleValues,
 		onChange,
 	});
 	return (
-		<Container>
+		<>
 			{items.map((item) => (
 				<Option
 					{...item}
+					{...rest}
 					key={item.name}
 					isChecked={isChecked(item.value)}
 					onPress={handlePress}
 				/>
 			))}
-		</Container>
+		</>
 	);
 };
-
-const Container = styled.View``;
--- src/components/commons/Check/components/Option.tsx
+++ src/components/commons/Check/components/Option.tsx
@@ -1,27 +1,30 @@
 import styled, { css } from "styled-components/native";
 
-import { Typography } from "../../Typography";
+import { BaseButton, BaseButtonProps } from "../../Buttons";
 import type { Modifiers } from "@/@types/modifiers";
 
-export type OptionProps = ButtonProps & {
-	name: string;
-	value: string;
-	onPress?: (value: string) => void;
-};
+export type OptionProps = ButtonProps &
+	Pick<BaseButtonProps, "style"> & {
+		name: string;
+		value: string;
+		onPress?: (value: string) => void;
+	};
 
 export const Option = (props: OptionProps) => {
-	const { name, value, isChecked, onPress } = props;
+	const { name, value, isChecked, onPress, ...rest } = props;
 	return (
 		<Button
+			{...rest}
 			onPress={onPress && (() => onPress(value))}
 			isChecked={isChecked}
+			secondary={!isChecked}
 			accessibilityLabel={name}
 			accessibilityHint={`Selecionará a opção com o valor ${name}`}
 			accessibilityRole="checkbox"
 			accessibilityState={{ checked: isChecked }}
 			testID="check-option"
 		>
-			<Text>{name}</Text>
+			{name}
 		</Button>
 	);
 };
@@ -33,10 +36,16 @@ const modifiers: Modifiers<keyof ButtonProps> = {
 	`,
 };
 
-const Button = styled.TouchableOpacity<ButtonProps>`
+const Button = styled(BaseButton)<ButtonProps>`
 	${({ theme, isChecked }) => css`
+		width: auto;
+		max-width: 140px;
+		min-width: auto;
+		height: 44px;
+		max-height: 44px;
+		min-height: 44px;
+		border-radius: ${theme.rounded.regular};
+		padding: ${theme.spaces[1]} ${theme.spaces[3]};
 		${isChecked && modifiers.isChecked(theme)};
 	`}
 `;
-
-const Text = styled(Typography.Paragraph)``;
