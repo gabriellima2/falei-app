78c857e0769a083eeae45b3243b7411c476f9cad
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-11-02 17:56:39 -0300

    fix: fixes styled-components import

--- src/components/Appointments/BreathingExerciseAppointments/BreathingExerciseAppointments.tsx
+++ src/components/Appointments/BreathingExerciseAppointments/BreathingExerciseAppointments.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from "styled-components";
+import { useTheme } from "styled-components/native";
 
 import { EmptyAppointments, Appointment } from "./components";
 import { HorizontalList } from "@/components/commons";
