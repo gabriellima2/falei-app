6e6346508c8d132eff385aa12569f78b011d33e8
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-11-02 17:50:05 -0300

    test: improves code organization and remove repetitions in test of use-check

--- src/components/commons/Check/hooks/use-check.test.ts
+++ src/components/commons/Check/hooks/use-check.test.ts
@@ -6,6 +6,10 @@ const executeHook = (params = defaultParams) =>
 	renderHook(() => useCheck(params));
 
 describe("UseCheck", () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
 	describe("Return", () => {
 		it("should return correctly values", () => {
 			const { result } = executeHook();
@@ -18,54 +22,61 @@ describe("UseCheck", () => {
 		const OTHER_VALUE = "other_value";
 		describe("HandlePress", () => {
 			const params = { ...defaultParams, onChange: jest.fn() };
-			it("should remove the passed value if it is already added", () => {
+			const cases = [
+				{
+					params,
+					value: params.initialValue,
+					expected: [],
+					description: "should remove the passed value if it is already added",
+				},
+				{
+					params,
+					value: OTHER_VALUE,
+					expected: [OTHER_VALUE],
+					description: "should add the new value by removing the previous one",
+				},
+				{
+					params: { ...params, multipleValues: true },
+					value: OTHER_VALUE,
+					expected: [params.initialValue, OTHER_VALUE],
+					description: "should add the new value keeping the previous ones",
+				},
+			];
+			test.each(cases)("%s", ({ params, value, expected }) => {
 				const { result } = executeHook(params);
 
 				act(() => {
-					result.current.handlePress(params.initialValue);
+					result.current.handlePress(value);
 				});
+				const { multipleValues, initialValue, onChange } = params;
 
-				expect(result.current.values).toMatchObject([]);
-				expect(params.onChange).toHaveBeenCalledWith([params.initialValue]);
-			});
-			it("should add the new value by removing the previous one", () => {
-				const { result } = executeHook(params);
-
-				act(() => {
-					result.current.handlePress(OTHER_VALUE);
-				});
-
-				expect(result.current.values).toMatchObject([OTHER_VALUE]);
-				expect(params.onChange).toHaveBeenCalledWith([OTHER_VALUE]);
-			});
-			it("should add the new value keeping the previous ones", () => {
-				const { result } = executeHook({ ...params, multipleValues: true });
-
-				act(() => {
-					result.current.handlePress(OTHER_VALUE);
-				});
-
-				expect(result.current.values).toMatchObject([
-					params.initialValue,
-					OTHER_VALUE,
-				]);
-				expect(params.onChange).toHaveBeenCalledWith([OTHER_VALUE]);
+				expect(result.current.values).toMatchObject(expected);
+				expect(onChange).toHaveBeenCalledWith(
+					multipleValues ? [initialValue, value] : [value]
+				);
 			});
 		});
 		describe("IsChecked", () => {
-			it("should return true if the value is already added", () => {
+			const cases = [
+				{
+					value: defaultParams.initialValue,
+					expected: true,
+					description: "should return true if the value is already added",
+				},
+				{
+					value: OTHER_VALUE,
+					expected: false,
+					description: "should return false if the value is not added",
+				},
+			];
+			test.each(cases)("%s", ({ value, expected }) => {
 				const { result } = executeHook();
 
-				const isChecked = result.current.isChecked(defaultParams.initialValue);
+				const isChecked = result.current.isChecked(value);
 
-				expect(isChecked).toBeTruthy();
-			});
-			it("should return false if the value is not added", () => {
-				const { result } = executeHook();
-
-				const isChecked = result.current.isChecked(OTHER_VALUE);
-
-				expect(isChecked).toBeFalsy();
+				expected
+					? expect(isChecked).toBeTruthy()
+					: expect(isChecked).toBeFalsy();
 			});
 		});
 	});
