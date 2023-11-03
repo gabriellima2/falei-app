f595188bcd9206c2b15beb2466f7b571ff2c18b5
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-11-03 18:04:59 -0300

    chore: improves styles of empty-appointments component

--- src/components/Appointments/BreathingExerciseAppointments/components/EmptyAppointments.tsx
+++ src/components/Appointments/BreathingExerciseAppointments/components/EmptyAppointments.tsx
@@ -1,9 +1,32 @@
+import styled, { css } from "styled-components/native";
+
 import { Typography } from "@/components/commons";
+import { dimensions } from "@/constants/dimensions";
 
 export const EmptyAppointments = () => {
 	return (
-		<Typography.Title testID="empty-data-message">
-			Não há lembretes para essa semana !
-		</Typography.Title>
+		<Container>
+			<Message testID="empty-data-message">
+				Você está livre de lembretes para essa semana
+			</Message>
+		</Container>
 	);
 };
+
+const Container = styled.View`
+	${({ theme }) => css`
+		width: ${dimensions.screen.withMargin.width}px;
+		flex: 1;
+		border-radius: ${theme.rounded.md};
+		padding: ${theme.spaces[3]};
+		border: 1px solid ${theme.colors.overlay};
+		background-color: ${theme.colors.utils.darkGray};
+	`}
+`;
+
+const Message = styled(Typography.Paragraph)`
+	${({ theme }) => css`
+		color: ${theme.colors.font.primary};
+		font-size: ${theme.fontSizes.regular};
+	`}
+`;
