5ad89c33278a2ba360cc0b2bbd824343ac843092
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-12-03 21:32:52 -0300

    fix: removes deprecated method of zod in user-auth validation

--- src/validations/user-auth-validation.ts
+++ src/validations/user-auth-validation.ts
@@ -1,13 +1,12 @@
 import * as z from "zod";
-import { UserAuthRequestDTO } from "@/dtos";
+import { AuthInputDTO } from "@/dtos/auth.dto";
 
-export const userAuthSchema: z.ZodType<UserAuthRequestDTO> = z.object({
+export const userAuthSchema: z.ZodType<AuthInputDTO> = z.object({
 	email: z
 		.string()
 		.min(1, { message: "O campo email é obrigatório" })
 		.email("Formato de email inválido"),
 	password: z
 		.string()
-		.nonempty({ message: "O campo senha é obrigatório" })
 		.min(8, { message: "Digite uma senha com 8 ou mais caracteres" }),
 });
