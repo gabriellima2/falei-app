912b4095d221c99b7db85cb9ea3b374171353be0
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-12-11 18:24:56 -0300

    test: add interactions test for exercises template

--- src/ui/templates/private-templates/Exercises/Exercises.test.tsx
+++ src/ui/templates/private-templates/Exercises/Exercises.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from "@testing-library/react-native";
+import { fireEvent, screen } from "@testing-library/react-native";
 
 import { Exercises } from "./Exercises";
 import * as ExercisesState from "./hooks/use-exercises-state";
@@ -6,18 +6,20 @@ import * as ExercisesState from "./hooks/use-exercises-state";
 import { renderWithThemeProvider } from "@/__mocks__/render-with-theme-provider";
 import { ExerciseCategoryEntity } from "@/entities/exercise-category.entity";
 import { BreathingExerciseEntity } from "@/entities/breathing-entities";
+import { categoriesPortuguese } from "@/constants/categories-portuguese";
 
 const useExercisesStateSpy = jest.spyOn(ExercisesState, "useExercisesState");
 
 const renderComponent = () => renderWithThemeProvider(<Exercises />);
 
 describe("<Exercises />", () => {
+	const defaultCategory = ExerciseCategoryEntity.Breathing;
 	const useExercisesStateDefaultReturn: ExercisesState.UseExercisesStateReturn =
 		{
 			isLoading: false,
 			exercises: undefined,
 			error: undefined,
-			category: ExerciseCategoryEntity.Breathing,
+			category: defaultCategory,
 			handleCategoryChange: jest.fn(),
 		};
 
@@ -76,10 +78,37 @@ describe("<Exercises />", () => {
 			});
 			renderComponent();
 
+			const categoryTitle = categoriesPortuguese[defaultCategory];
+
 			expectStaticElsToHaveBeenPresent();
 			expect(getExerciseEls()).toHaveLength(EXERCISES.length);
+			expect(screen.getAllByText(categoryTitle)[1]).toBeTruthy();
 			expect(getLoadingEl()).toBeFalsy();
 			expect(getErrorEl()).toBeFalsy();
+		});
+	});
+	describe("Interactions", () => {
+		describe("Press", () => {
+			describe("HandleCategoryChange", () => {
+				const getCategoryOptionEls = () =>
+					screen.getAllByTestId("check-option");
+
+				it("should call the 'handleCategoryChange' function when category has changed", () => {
+					const mockHandleCategoryChange = jest.fn();
+					useExercisesStateSpy.mockReturnValue({
+						...useExercisesStateDefaultReturn,
+						handleCategoryChange: mockHandleCategoryChange,
+					});
+					renderComponent();
+
+					const category = ExerciseCategoryEntity.TongueTwister;
+					const [, option] = getCategoryOptionEls();
+					fireEvent.press(option);
+
+					expect(mockHandleCategoryChange).toHaveBeenCalled();
+					expect(mockHandleCategoryChange).toHaveBeenCalledWith(category);
+				});
+			});
 		});
 	});
 });
