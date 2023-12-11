c91eb7035b91d860c906ca3e668290b89818d6a6
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-12-11 17:14:44 -0300

    test: add tests for settings template

---
+++ src/ui/templates/private-templates/Settings/Settings.test.tsx
@@ -0,0 +1,48 @@
+import { fireEvent, screen } from "@testing-library/react-native";
+
+import { Settings } from "./Settings";
+import * as SettingsState from "./hooks/use-settings-state";
+
+import { renderWithThemeProvider } from "@/__mocks__/render-with-theme-provider";
+
+jest.mock("@/helpers/firebase-auth", () => ({
+	firebaseAuth: {
+		signOut: jest.fn(),
+	},
+}));
+
+const useSettingsStateSpy = jest.spyOn(SettingsState, "useSettingsState");
+
+const renderComponent = () => renderWithThemeProvider(<Settings />);
+
+describe("<Settings />", () => {
+	const getLogoutButtonEl = () => screen.getByLabelText("Sair");
+
+	describe("Render", () => {
+		it("should render correctly", () => {
+			renderComponent();
+
+			expect(getLogoutButtonEl()).toBeTruthy();
+			expect(screen.getByText("Configurações")).toBeTruthy();
+			expect(screen.getByText("© Gabriel Lima")).toBeTruthy();
+		});
+	});
+	describe("Interactions", () => {
+		describe("Press", () => {
+			describe("HandleLogout", () => {
+				it("should call 'handleLogout' function when pressed on LogoutButton", () => {
+					const mockHandleLogout = jest.fn();
+					useSettingsStateSpy.mockReturnValue({
+						handleLogout: mockHandleLogout,
+					});
+					renderComponent();
+
+					const button = getLogoutButtonEl();
+					fireEvent.press(button);
+
+					expect(mockHandleLogout).toHaveBeenCalled();
+				});
+			});
+		});
+	});
+});
