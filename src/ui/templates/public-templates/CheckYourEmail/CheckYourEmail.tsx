b33a4e0f93cbda9737f830abac4ad4ef27e539b2
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-01-11 17:46:15 -0300

    feat: add structure of check-your-email template

--- src/ui/templates/public-templates/CheckYourEmail/CheckYourEmail.tsx
+++ src/ui/templates/public-templates/CheckYourEmail/CheckYourEmail.tsx
@@ -1,9 +1,79 @@
-import { ContainerWithDefaultSpaces, Typography } from "@/ui/atoms";
+import { Linking } from "react-native";
+import styled, { css } from "styled-components/native";
+
+import {
+	BaseButton,
+	ButtonLink,
+	Typography,
+	ContainerWithDefaultSpaces,
+} from "@/ui/atoms";
 
 export const CheckYourEmail = () => {
 	return (
-		<ContainerWithDefaultSpaces horizontalSpacing verticalSpacing>
-			<Typography.Title>Check Your Email...</Typography.Title>
-		</ContainerWithDefaultSpaces>
+		<Container horizontalSpacing verticalSpacing>
+			<Content>
+				<Image
+					resizeMode="contain"
+					source={require("../../../../../public/assets/sended-email.png")}
+				/>
+				<TextContainer>
+					<Title>Verifique o seu e-mail</Title>
+					<Description>
+						Foi enviado para o seu endereço de e-mail um link para você criar
+						uma nova senha.
+					</Description>
+				</TextContainer>
+			</Content>
+			<Footer>
+				<BaseButton onPress={() => Linking.openURL("mailto:")}>
+					Verificar seu endereço de e-mail
+				</BaseButton>
+				<ButtonLink onlyText href={{ pathname: "(auth)/login" }}>
+					Prefiro fazer isso mais tarde
+				</ButtonLink>
+			</Footer>
+		</Container>
 	);
 };
+
+const Container = styled(ContainerWithDefaultSpaces)`
+	flex: 1;
+`;
+
+const Content = styled.View`
+	${({ theme }) => css`
+		flex: 1;
+		align-items: center;
+		gap: ${theme.spaces[3]};
+	`}
+`;
+
+const Image = styled.Image`
+	width: 200px;
+	min-width: auto;
+	height: 150px;
+	min-height: auto;
+`;
+
+const TextContainer = styled.View`
+	${({ theme }) => css`
+		align-items: center;
+		gap: ${theme.spaces[3]};
+	`}
+`;
+
+const Title = styled(Typography.Title)`
+	text-align: center;
+`;
+
+const Description = styled(Typography.Paragraph)`
+	text-align: center;
+`;
+
+const Footer = styled.View`
+	${({ theme }) => css`
+		flex: 1;
+		align-items: center;
+		gap: ${theme.spaces[2]};
+	`}
+`;
