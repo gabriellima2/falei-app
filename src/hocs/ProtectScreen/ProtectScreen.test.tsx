52eb829ad56c684f89d845a4406989ea1b1bfdd2
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-01-12 15:24:44 -0300

    test: adapt protect-screen tests to work with authentication-store

--- src/hocs/ProtectScreen/ProtectScreen.test.tsx
+++ src/hocs/ProtectScreen/ProtectScreen.test.tsx
@@ -4,17 +4,20 @@ import * as Router from "expo-router";
 import { ProtectScreen } from "./ProtectScreen";
 import { PRIVATE_GROUP_NAME, PUBLIC_GROUP_NAME } from "./constants/group-names";
 
+import * as AuthenticationStore from "@/store/authentication-store/authentication.store";
 import { renderWithThemeProvider } from "@/__mocks__/render-with-theme-provider";
-import * as authStore from "@/store/authentication-store";
 import { mockReplace } from "jest-setup";
-import { UserEntity } from "@/entities/user.entity";
 
-jest.mock("@/store/auth-store", () => ({ useAuthStore: jest.fn() }));
+jest.mock("@/lib/firebase-auth", () => ({
+	firebaseAuth: {},
+}));
 
-const user = {
-	id: "1",
-	email: "any@email.com",
-} as Omit<UserEntity, "password">;
+const segmentsSpy = jest.spyOn(Router, "useSegments");
+const pathnameSpy = jest.spyOn(Router, "usePathname");
+const authenticationStoreSpy = jest.spyOn(
+	AuthenticationStore,
+	"useAuthenticationStore"
+);
 
 const renderComponent = () => {
 	const PrivateComponent = ProtectScreen(() => <Text>Any Text</Text>);
@@ -22,46 +25,40 @@ const renderComponent = () => {
 };
 
 describe("<ProtectScreen />", () => {
-	afterEach(() => {
+	const authenticationStoreMock = {
+		user: {
+			id: "1",
+			email: "any@email.com",
+		},
+		checkAuthState: jest.fn(),
+		authHasBeenChecked: true,
+		isNewUser: false,
+		signOut: jest.fn(),
+		anonymous: jest.fn(),
+		resetPassword: jest.fn(),
+		signIn: jest.fn(),
+		signUp: jest.fn(),
+	};
+
+	beforeEach(() => {
 		jest.clearAllMocks();
 	});
 
 	it("should redirect when user is authenticated and access a public route", () => {
-		jest.spyOn(Router, "useSegments").mockReturnValue(["any"]);
-		jest.spyOn(Router, "usePathname").mockReturnValue("/");
-		jest.spyOn(authStore, "useAuthenticationStore").mockImplementation((fn) =>
-			fn({
-				user,
-				checkAuthState: jest.fn(),
-				authHasBeenChecked: true,
-				isNewUser: false,
-				signOut: jest.fn(),
-				anonymous: jest.fn(),
-				resetPassword: jest.fn(),
-				signIn: jest.fn(),
-				signUp: jest.fn(),
-			})
-		);
+		segmentsSpy.mockReturnValue(["any"]);
+		pathnameSpy.mockReturnValue("/");
+		authenticationStoreSpy.mockReturnValue({ ...authenticationStoreMock });
 		renderComponent();
 
 		expect(mockReplace).toHaveBeenCalledWith(`/${PRIVATE_GROUP_NAME}/`);
 	});
 	it("should redirect when user is not authenticated and access a private route", () => {
-		jest.spyOn(Router, "useSegments").mockReturnValue([PRIVATE_GROUP_NAME]);
-		jest.spyOn(Router, "usePathname").mockReturnValue("/any");
-		jest.spyOn(authStore, "useAuthenticationStore").mockImplementation((fn) =>
-			fn({
-				user: null,
-				checkAuthState: jest.fn(),
-				authHasBeenChecked: true,
-				isNewUser: false,
-				signOut: jest.fn(),
-				anonymous: jest.fn(),
-				resetPassword: jest.fn(),
-				signIn: jest.fn(),
-				signUp: jest.fn(),
-			})
-		);
+		segmentsSpy.mockReturnValue([PRIVATE_GROUP_NAME]);
+		pathnameSpy.mockReturnValue("/any");
+		authenticationStoreSpy.mockReturnValue({
+			...authenticationStoreMock,
+			user: null,
+		});
 		renderComponent();
 
 		expect(mockReplace).toHaveBeenCalledWith(`/${PUBLIC_GROUP_NAME}/login`);
