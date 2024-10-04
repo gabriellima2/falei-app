1e16a43b168d025cdcd28aa5918c5551abf7743a
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-10-04 16:52:20 -0300

    feat: add loading treatment into read tongue-twister and poem

--- src/ui/templates/home.template/components/poems/components/read-poem-bottom-sheet.tsx
+++ src/ui/templates/home.template/components/poems/components/read-poem-bottom-sheet.tsx
@@ -1,43 +1,27 @@
-import { useMemo } from 'react'
-import { View } from 'react-native'
+import { ActivityIndicator } from 'react-native'
 
 import { usePoemsContext } from '../contexts/poems.context/hooks'
 import { useGetPoemById } from '@/hooks/http/use-get-poem-by-id'
 
 import { BottomSheetScrollViewModal } from '@/ui/components/bottom-sheet/bottom-sheet-scroll-view-modal'
+import { PoemStanzas } from '@/ui/components/poem-stanzas'
 import { Typography } from '@/ui/atoms/typography'
-
-import { getPoemStanzas } from '@/helpers/general'
 
 export function ReadPoemBottomSheet() {
 	const { poemId, readPoemBottomSheetRef } = usePoemsContext()
-	const { poem } = useGetPoemById(poemId)
-
-	const poemStanzas = useMemo(() => {
-		if (!poem) return []
-		return getPoemStanzas(poem.body)
-	}, [poem])
-
+	const { poem, isLoading } = useGetPoemById(poemId)
 	return (
 		<BottomSheetScrollViewModal ref={readPoemBottomSheetRef}>
-			<Typography.Title>
-				{poem?.title}
-			</Typography.Title>
-			<Typography.Paragraph className="text-base-text-muted mt-1">
-				Autor: {poem?.authorName || 'Não informado'}
-			</Typography.Paragraph>
-			{poemStanzas.map((stanza, index) => (
-				<View key={index} className="mt-4">
-					{stanza.map((verse, index) => (
-						<Typography.Paragraph
-							key={`${verse}-${index}`}
-							className="text-base"
-						>
-							{verse.trim()}
-						</Typography.Paragraph>
-					))}
-				</View>
-			))}
+			{poem && (
+				<>
+					<Typography.Title>{poem.title}</Typography.Title>
+					<Typography.Paragraph className="text-base-text-muted mt-1">
+						Autor: {poem.authorName || 'Não informado'}
+					</Typography.Paragraph>
+					<PoemStanzas poemText={poem.body} />
+				</>
+			)}
+			{!poem && isLoading && <ActivityIndicator />}
 		</BottomSheetScrollViewModal>
 	)
 }
--- src/ui/templates/home.template/components/tongues-twister/components/read-tongue-twister-bottom-sheet.tsx
+++ src/ui/templates/home.template/components/tongues-twister/components/read-tongue-twister-bottom-sheet.tsx
@@ -1,32 +1,37 @@
 import { useMemo } from 'react'
+import { ActivityIndicator } from 'react-native'
 
 import { useTonguesTwisterContext } from '../contexts/tongues-twister.context/hooks'
 import { useGetTongueTwisterById } from '@/hooks/http/use-get-tongue-twister-by-id'
 
 import { BottomSheetScrollViewModal } from '@/ui/components/bottom-sheet/bottom-sheet-scroll-view-modal'
+import { Paragraphs } from '@/ui/components/paragraphs'
 import { Typography } from '@/ui/atoms/typography'
 
 import { getSentencesWithBreakLine } from '@/helpers/general'
 
 export function ReadTongueTwisterBottomSheet() {
 	const { tongueTwisterId, readTongueTwisterBottomSheetRef } = useTonguesTwisterContext()
-	const { tongueTwister } = useGetTongueTwisterById(tongueTwisterId)
+	const { tongueTwister, isLoading } = useGetTongueTwisterById(tongueTwisterId)
 
-	const sentences = useMemo(() => {
+	const tongueTwisterSentences = useMemo(() => {
 		if (!tongueTwister) return []
 		return getSentencesWithBreakLine(tongueTwister.body)
 	}, [tongueTwister])
 
+	const hasTongueTwisterSentences = !!tongueTwisterSentences.length
+
 	return (
 		<BottomSheetScrollViewModal ref={readTongueTwisterBottomSheetRef}>
-			{sentences.map((sentence, index) => (
-				<Typography.Paragraph key={index} className="text-base">
-					{sentence.trim()}
-				</Typography.Paragraph>
-			))}
-			<Typography.Paragraph className="text-base-text-muted mt-4">
-				Autor: {tongueTwister?.authorName || 'Não informado'}
-			</Typography.Paragraph>
+			{hasTongueTwisterSentences && (
+				<>
+					<Paragraphs paragraphs={tongueTwisterSentences} />
+					<Typography.Paragraph className="text-base-text-muted mt-4">
+						Autor: {tongueTwister?.authorName || 'Não informado'}
+					</Typography.Paragraph>
+				</>
+			)}
+			{!hasTongueTwisterSentences && isLoading && <ActivityIndicator />}
 		</BottomSheetScrollViewModal>
 	)
 }
