56e943734e34e81f255f4d25cc36e405593a9fde
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-11-05 19:39:54 -0300

    feat: show confetti on complete breathing-exercise

--- src/ui/templates/exercise-completed.template/index.tsx
+++ src/ui/templates/exercise-completed.template/index.tsx
@@ -1,6 +1,7 @@
 import { useCallback } from 'react'
 import { View } from 'react-native'
 import { Medal } from 'lucide-react-native'
+import { Confetti } from 'react-native-fast-confetti'
 
 import { Button } from '@/ui/atoms/buttons/button'
 import { Typography } from '@/ui/atoms/typography'
@@ -19,20 +20,23 @@ export function ExerciseCompletedTemplate() {
 	}, [navigation])
 
 	return (
-		<Container>
-			<View className="flex-1 items-center justify-center">
-				<View className="items-center justify-center">
-					<Medal size={40} color={colors.base.primary} strokeWidth={1.3} />
-					<Typography.Title className="text-center mt-4 text-xl">
-						Concluído com sucesso!
-					</Typography.Title>
+		<>
+			<Confetti autoplay fadeOutOnEnd count={300} />
+			<Container>
+				<View className="flex-1 items-center justify-center">
+					<View className="items-center justify-center">
+						<Medal size={40} color={colors.base.primary} strokeWidth={1.3} />
+						<Typography.Title className="text-center mt-4 text-xl">
+							Concluído com sucesso!
+						</Typography.Title>
+					</View>
+					<Typography.Paragraph className="text-center max-w-[300px] mt-2">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt.
+					</Typography.Paragraph>
 				</View>
-				<Typography.Paragraph className="text-center max-w-[300px] mt-2">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt.
-				</Typography.Paragraph>
-			</View>
-			<Button label="Confirmar" onPress={handleConfirm} />
-		</Container>
+				<Button label="Confirmar" onPress={handleConfirm} />
+			</Container>
+		</>
 	)
 }
