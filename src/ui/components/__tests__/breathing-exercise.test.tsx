e7785f973e50053fdb13662c8dfe8982b2aa4659
Gabriel Lima <gabriel.rslima10@gmail.com>
2025-01-19 16:40:31 -0300

    test: add breathing-exercise test

---
+++ src/ui/components/__tests__/breathing-exercise.test.tsx
@@ -0,0 +1,57 @@
+import { fireEvent, render, screen } from '@testing-library/react-native'
+import { BreathingExercise } from '../breathing-exercise'
+
+describe('<BreathingExercise />', () => {
+	describe('Duration time', () => {
+		it('should display the correct duration in seconds', () => {
+			renderComponent()
+
+			screen.getByText('42 seg.')
+		})
+		it('should display the correct duration in minutes when total time is 60 seconds or more', () => {
+			renderComponent({
+				...defaultProps,
+				steps: { exhale: 10000, hold: 10000, inhale: 10000 },
+			})
+
+			screen.getByText('1 min.')
+		})
+	})
+	describe('Actions', () => {
+		it('should invoke the callback function when the menu is pressed', () => {
+			renderComponent()
+
+			const menu = screen.getByTestId('exercise-menu')
+			fireEvent.press(menu)
+
+			expect(defaultProps.onMenuPress).toHaveBeenCalledTimes(1)
+			expect(defaultProps.onMenuPress).toHaveBeenCalledWith(defaultProps.id)
+		})
+		it('should invoke the callback function when the exercise is pressed', () => {
+			renderComponent()
+
+			const exercise = screen.getByTestId('exercise-root')
+			fireEvent.press(exercise)
+
+			expect(defaultProps.onPress).toHaveBeenCalledTimes(1)
+			expect(defaultProps.onPress).toHaveBeenCalledWith(defaultProps.id)
+		})
+	})
+})
+
+const defaultProps: Parameters<typeof BreathingExercise>[0] = {
+	id: '1',
+	roundsTotal: 3,
+	steps: {
+		exhale: 5000,
+		hold: 4000,
+		inhale: 5000
+	},
+	title: 'any_title',
+	onMenuPress: jest.fn(),
+	onPress: jest.fn(),
+}
+
+function renderComponent(props = defaultProps) {
+	return render(<BreathingExercise {...props} />)
+}
--- src/ui/components/base-exercise.tsx
+++ src/ui/components/base-exercise.tsx
@@ -67,6 +67,7 @@ function Root(props: RootProps) {
 	return (
 		<BaseExerciseContext.Provider value={{ variant, onMenuPress }}>
 			<TouchableOpacity
+				testID="exercise-root"
 				onPress={onPress}
 				activeOpacity={0.9}
 				className={cn(
@@ -157,7 +158,7 @@ function Paragraph(props: TextProps) {
 function Menu() {
 	const { onMenuPress } = useBaseExerciseContext()
 	return (
-		<TouchableOpacity onPress={onMenuPress} activeOpacity={0.8} hitSlop={10}>
+		<TouchableOpacity testID='exercise-menu' onPress={onMenuPress} activeOpacity={0.8} hitSlop={10}>
 			<EllipsisVertical color={colors.white} />
 		</TouchableOpacity>
 	)
